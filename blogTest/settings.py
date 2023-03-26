d558e1cbedd2159e06cff50af7c578b78279805b
zhengpeibei <zhengpeibei@163.com>
2023-03-26 19:00:39 +0800

    配置MySQL数据库

--- blogTest/settings.py
+++ blogTest/settings.py
@@ -74,12 +74,23 @@ WSGI_APPLICATION = 'blogTest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',#数据库主机
+        'PORT': 3306,#数据库端口
+        'USER': 'root',#数据库用户
+        'PASSWORD':'123456',#数据库密码
+        'NAME': 'blogtestdb',#数据库名称
     }
 }
+
 
 
 # Password validation
